96483604038fe4e2b49249a8439d30fb626bf90c
Ryan Harricharan <138528052+rharricharan@users.noreply.github.com>
2025-05-10 15:57:29 -0400

    Initial commit

---
+++ paire/app/(tabs)/search.tsx
@@ -0,0 +1,11 @@
+import { ThemedView } from "@/components/ThemedView";
+import React from "react";
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+
+export default function SearchPage() {
+    return (
+        <ThemedView>
+
+        </ThemedView>
+    )
+}
