5a39e4a66922ea9672689da4c49074fdebbcba20
Ryan Harricharan <138528052+rharricharan@users.noreply.github.com>
2025-05-11 23:48:03 -0400

    updated commit 2

--- paire/app/(tabs)/search.tsx
+++ paire/app/(tabs)/search.tsx
@@ -1,11 +1,28 @@
-import { ThemedView } from "@/components/ThemedView";
 import React from "react";
-import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { Text, ScrollView, View, TextInput } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SearchPage() {
     return (
-        <ThemedView>
-
-        </ThemedView>
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView contentContainerStyle={{paddingHorizontal: 16 }}>
+                <View style={{
+                    flex: 1,
+                    width: "100%",
+                }}>
+                    <Text>Search</Text>
+                    <TextInput
+                        style={{
+                            borderWidth: 1,
+                            borderColor: "#ccc",
+                            borderRadius: 8,
+                            padding: 12,
+                            marginTop: 8
+                        }}
+                        placeholder="Search"
+                    />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
